664069e38a2ee05112072eb02fc8bf18650ca400
CursedMun <Frosty.HyperX@gmail.com>
2021-03-23 12:36:52 +0000

    updates

--- src/dependecy/FreyaClient.ts
+++ src/dependecy/FreyaClient.ts
@@ -1,6 +1,7 @@
 import { Client, Collection, Message } from "discord.js";
 import { inject, injectable } from "inversify";
 import { Logger } from "../helpers/Logger";
+import { CommandHandler } from "../services/Command-handler";
 import { MessageResponder } from "../services/Message-responder";
 import { FreyaClient } from "../structure/FreyaClient";
 import { Global } from "./Global";
@@ -22,6 +23,7 @@ export class Bot {
   }
 
   public startUp(): Promise<string> {
+    CommandHandler(this)
     this.client.on('message', (message: Message) => {
       if (message.author.bot) {
         Logger.log('Ignoring bot message!')
--- src/structure/FreyaClient.ts
+++ src/structure/FreyaClient.ts
@@ -1,10 +1,11 @@
-import { Client } from "discord.js";
+import { Client, Collection, DiscordAPIError } from "discord.js";
 import { FileType, IFreyaOptions } from '../helpers/Types';
 import { Store } from "./Store";
 export class FreyaClient extends Client {
  
     public folders: { [key: string]: {path: string , files:string[]}};
     public prefix: "!" | ".";
+    public commands: Collection<string,Object>;
     constructor(opts: IFreyaOptions, token: string = "") {
         var myOptions = {
             prefix: opts.prefix,
